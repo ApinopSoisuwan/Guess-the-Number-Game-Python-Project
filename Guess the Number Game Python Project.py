b9a4e8636b66b74f39e98e058302fa4cd858f092
Apinop <apinop.soisuwan@gmail.com>
2021-12-01 21:44:48 +0700

    adding try except fix error

--- Guess the Number Game Python Project.py
+++ Guess the Number Game Python Project.py
@@ -1,23 +1,31 @@
 def guess_num ():
     import random
-    #set range 0 - 100
-    guess = random.randrange(0,100)
+    #set range 0 - 10
+    guess = random.randrange(0,10)
     coin = 0
-    while coin <= 1 :
-        try_num = input("\n"+'Guess the Number (1 - 100):')
-        if try_num.isalpha():
-            print("\n"+"Insert Number")
-        if guess > int(try_num):
-            print("\n"+'Higher')
-        elif guess < int(try_num):
-            print("\n"+'Lower')
-        elif guess == int(try_num):
-            print("\n"+"Correct")
-            play_again = input("Play Again (y/n)")
-            if play_again == "y" or play_again == "Y":
-                print(guess_num())
-            elif play_again == "n" or  play_again == "N":
+    coin_continue = 0
+    try:
+        while coin <= 1 :
+            try_num = input("\n"+'Guess the Number (1 - 10):')
+            if guess > int(try_num):
+                print("\n"+'Higher')
+            elif guess < int(try_num):
+                print("\n"+'Lower')
+            elif guess == int(try_num):
                 coin += 1
-                return "Exit"
+                print("\n" + "Correct")
+                play_again = input("Play Again (y/n): ")
+                while coin_continue <= 1:
+                    if play_again.isdigit() :
+                        print(play_again)
+                    elif play_again == "y" or play_again == "Y":
+                        print(guess_num())
+                    elif play_again == "n" or play_again == "N":
+                        coin += 1
+                        return "Exit"
+    except:
+        print('Insert Number Only')
+        return guess_num()
+
 
 print(guess_num())
